89a1c2a0b8ee43381183f0f2af5593e686ce1e00
NIPI233 <2392959673@qq.com>
2020-10-12 13:40:29 +0800

    test2_2020_10

---
+++ src/test2/test.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class test {
+	public static void main(String[] args) {
+
+		String str = "qw";
+		if(str=="") {
+			System.out.println(str);
+		}
+		else {
+		System.out.println("111");
+		}
+	}
+
+}
